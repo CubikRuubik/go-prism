50536490275b5b006e64b05012628aa781754256
Oleh <cuubikruubik@gmail.com>
2026-04-07 15:58:05 +0300

    Revert "Added separate file & dependency"
    This reverts commit da1e9031850d692245fb0770dbe88f44d309e048.

--- formatter.go
+++
@@ -1,8 +0,0 @@
-package main
-
-import "github.com/fatih/color"
-
-func printColored(label, message string) {
-	color.Green("[%s] ", label)
-	color.Yellow(message)
-}
--- main.go
+++ main.go
@@ -6,5 +6,4 @@ func main() {
     fmt.Println("Hello, World!")
     fmt.Println("First Commit")
     fmt.Println("Second Commit")
-    printColored("info", "colored output via fatih/color")
 }
