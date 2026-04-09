57721af4bf060531d38c463921d0b9f36a14fe4e
Oleh <cuubikruubik@gmail.com>
2026-04-09 17:57:45 +0300

    Added queue type

--- main.go
+++ main.go
@@ -6,4 +6,17 @@ import (
 
 func main() {
 	fmt.Println("Hello, World!")
+
+	q := NewQueue()
+	q.Enqueue(10)
+	q.Enqueue(20)
+	q.Enqueue(30)
+
+	fmt.Printf("Queue size: %d\n", q.Size())
+
+	for _, item := range q.Drain() {
+		fmt.Printf("  [%s] value=%d\n", item.ID, item.Value)
+	}
+
+	fmt.Printf("Queue size after drain: %d\n", q.Size())
 }
