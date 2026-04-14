4fce7d73f0a451b8ae9e74743bc0c8f3718e231e
Oleh <cuubikruubik@gmail.com>
2026-04-14 18:17:24 +0300

    Added entity

--- main.go
+++ main.go
@@ -1,6 +1,20 @@
 package main
 
+import (
+	"fmt"
+	"time"
+)
+
 func main() {
-	println("Hello world!");
-	println("new print");
+	task := NewTask(
+		"Implement prism core",
+		"Build the main refraction logic for go-prism",
+		PriorityHigh,
+		time.Now().Add(48*time.Hour),
+	)
+
+	fmt.Println(task)
+
+	task.Complete()
+	fmt.Println("After completion:", task)
 }
