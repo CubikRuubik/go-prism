28f496c2c1ac7307a0e91d36222c4889d978e5be
Oleh <cuubikruubik@gmail.com>
2026-04-15 10:49:36 +0300

    clean up

--- main.go
+++ main.go
@@ -1,20 +1,6 @@
 package main
 
-import (
-	"fmt"
-	"time"
-)
-
 func main() {
-	task := NewTask(
-		"Implement prism core",
-		"Build the main refraction logic for go-prism",
-		PriorityHigh,
-		time.Now().Add(48*time.Hour),
-	)
 
-	fmt.Println(task)
-
-	task.Complete()
-	fmt.Println("After completion:", task)
+	println("Hello, World!")
 }
